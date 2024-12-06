dddc5b22bfc11a772a4f5f7e29660e61b3b10b3c
Mert Bulan <mertbulan@icloud.com>
2024-12-06 15:59:26 +0100

    Initial Commit

---
+++ PingMe/PingMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingMeApp.swift
+//  PingMe
+//
+//  Created by Mert Bulan on 06.12.24.
+//
+
+import SwiftUI
+
+@main
+struct PingMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
